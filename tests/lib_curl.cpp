f51c2bf1e64ddd624efdf089ded2d87fe7e28285
TaimWay <taimway@gmail.com>
2026-04-19 16:07:37 +0800

    Test update

---
+++ tests/lib_curl.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <curl/curl.h>
+
+int main() {
+    CURL *curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://httpbin.org/get");
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    return 0;
+}
+
